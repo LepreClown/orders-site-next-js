a752df841e922961f02aad1d73db91f8efc320fc
LepreClown <serpuhov2002@mail.ru>
2023-07-06 18:06:33 +0300

    fix scss admin table

--- app/components/ui/admin-table/AdminTable/AdminTableHeader.tsx
+++ app/components/ui/admin-table/AdminTable/AdminTableHeader.tsx
@@ -9,12 +9,12 @@ const AdminTableHeader: FC<{ headerItems: string[] }> = ({ headerItems }) => {
 	return (
 		<div className={cn(styles.itemHeader)}>
 			{headerItems.map((value) => (
-				<>
-					<div key={value}>{value} </div>
+				<div key={value}>
+					<div>{value}</div>
 					<div className={styles.sort}>
 						<MaterialIconTi name="TiArrowUnsorted" />
 					</div>
-				</>
+				</div>
 			))}
 
 			<div>Действия</div>
