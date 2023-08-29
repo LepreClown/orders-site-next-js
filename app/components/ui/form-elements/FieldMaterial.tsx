b64b3b930fc692ef3f2e97ec95e7ca4a3def15ee
LepreClown <serpuhov2002@mail.ru>
2023-08-29 16:58:26 +0300

    fix netlify v2.1

--- app/components/ui/form-elements/FieldMaterial.tsx
+++ app/components/ui/form-elements/FieldMaterial.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from 'react'
 import { Control, Controller, FieldErrors, UseFormRegister } from 'react-hook-form'
 
+import { IOrderEditFields, IOrderEditInput } from '@/screens/admin/order/order-edit-interface'
+import { IOrderCreate } from '@/screens/admin/orders/orders.interface'
+import { IOrderEditInputAdvanced } from '@/screens/order/orderAdvanced/order-advanced-edit-interface'
+
 import Field from '@/ui/form-elements/Field'
 import MaterialIcon from '@/ui/icons/MaterialIcon'
 
@@ -9,7 +13,7 @@ import { IMaterials } from '@/shared/types/orders.types'
 import styles from './field-material.module.scss'
 
 interface IFieldMaterial {
-	errors: FieldErrors<any>
+	errors: FieldErrors<IOrderEditInputAdvanced | IOrderEditInput | IOrderCreate | IOrderEditFields>
 	register: UseFormRegister<any>
 	control: Control<any, any>
 	removeField: (index: number) => void
